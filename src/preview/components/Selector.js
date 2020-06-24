d124732aa895f33eae2757b53d688736d383365d
CircleCI <rob+platform-ui-build@hmn.md>
2020-06-24 12:49:35 +0000

    Build changes from 810a05f0dbbda768621710fb757e9c6a891f88fb
    https://circleci.com/gh/humanmade/aws-analytics/48

--- src/preview/components/Selector.js
+++ src/preview/components/Selector.js
@@ -1,9 +1,25 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Selector() {
 	const [ selected, setSelected ] = useState( Altis.Analytics.getAudiences() );
-	const audiences = window.AltisExperimentsPreview.audiences;
+	const {
+		audiences,
+		editLabel,
+		editUrl,
+		label,
+	} = window.AltisExperimentsPreview;
+
+	// Update the selected audiences on the first update event as
+	// the analytics ready event can fire before the audiences have been calculated.
+	useEffect( () => {
+		const listener = Altis.Analytics.on( 'updateAudiences', () => {
+			setSelected( Altis.Analytics.getAudiences() );
+		} );
+		return () => {
+			Altis.Analytics.off( listener );
+		};
+	}, [] );
 
 	const isSelected = id => selected.indexOf( id ) >= 0;
 	const onClick = ( e, id ) => {
@@ -27,7 +43,7 @@ export default function Selector() {
 				className="ab-item"
 				href="#qm-overview"
 			>
-				Audiences
+				{ label }
 			</a>
 			<div className="ab-sub-wrapper">
 				<ul className="ab-submenu">
@@ -46,6 +62,16 @@ export default function Selector() {
 							</a>
 						</li>
 					) ) }
+					{ audiences.length === 0 && (
+						<li>
+							<a
+								className="ab-item"
+								href={ editUrl }
+							>
+								{ editLabel }
+							</a>
+						</li>
+					) }
 				</ul>
 			</div>
 		</>
